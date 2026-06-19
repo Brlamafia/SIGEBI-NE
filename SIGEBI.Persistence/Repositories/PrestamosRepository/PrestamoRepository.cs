e907f2c71822eb75fe4e421defaea41cbd87b9d0
Stanley <stanleycamachoa@gmail.com>
2026-06-19 00:54:20 -0400

    Agregar nueva estructura al proyecto SIGEBI

--- SIGEBI.Persistence/Repositories/PrestamosRepository/PrestamoRepository.cs
+++ SIGEBI.Persistence/Repositories/PrestamosRepository/PrestamoRepository.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SIGEBI.Infrastructure.Repositories
+namespace SIGEBI.Persistence.Repositories.PrestamosRepository
 {
     internal class PrestamoRepository
     {
