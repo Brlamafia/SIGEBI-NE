bf6ac5ab347cbc7a9ccf2ad5346ef65711aea369
Stanley <stanleycamachoa@gmail.com>
2026-06-30 16:38:55 -0400

    Actualización final arquitectura proyecto SIGEBI

--- SIGEBI.Desktop/Form1.Designer.cs
+++ SIGEBI.Desktop/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WinFormsApp1
+﻿namespace SIGEBI.Desktop
 {
     partial class Form1
     {
--- SIGEBI.Desktop/Program.cs
+++ SIGEBI.Desktop/Program.cs
@@ -1,4 +1,4 @@
-namespace WinFormsApp1
+namespace SIGEBI.Desktop
 {
     internal static class Program
     {
